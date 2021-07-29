53c4495f22b9b6086c0835d23a3b96915b51361d
Simón Botero <sboteroarango@gmail.com>
2021-07-29 16:41:17 -0500

    Create rellenarMatriz.cs

---
+++ rellenarMatriz.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz = new int[5,5];
+            int longitud = matriz.GetLength(0);
+            for(int i=0; i<longitud;i++){
+                matriz[i,i] = 1;
+                matriz[i,(longitud-1)-i] = 2;
+
+            }
+            mostrarMatriz(matriz);
+            
+
+            
+        }
+        public static void mostrarMatriz(int[,] matriz){
+            int longitud = matriz.GetLength(0);
+            for(int i = 0; i<longitud; i++){
+                for(int j=0;j<longitud;j++){
+                    Console.Write(matriz[i,j]+" | ");
+                    
+                }
+            Console.WriteLine(" ");
+            }
+
+        }
+    }
